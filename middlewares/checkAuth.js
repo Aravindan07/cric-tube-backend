b7b4ab1f038ee6e018a0296e9528cab7a2d809c0
Aravindan <aravindanravi33@gmail.com>
2021-05-14 11:21:58 +0530

    fix: fixed header issue

--- middlewares/checkAuth.js
+++ middlewares/checkAuth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 function checkAuth(req, res, next) {
-	const token = req.header("x-auth-token") || req.headers.authorization.split(" ")[1];
+	const token = req.header("x-auth-token");
 	// const token = req.headers.authorization.split(" ")[1];
 
 	//Check for token
