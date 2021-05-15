7c31cf377dde8ddbe89bfd3725b9d3c5a15ba528
Aravindan <aravindanravi33@gmail.com>
2021-05-16 00:17:07 +0530

    fix: fixed dislike response

--- routes/dislikedVideos.router.js
+++ routes/dislikedVideos.router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const DislikedVideo = require("../models/dislikeVideo.model");
 const User = require("../models/user.model");
 const Video = require("../models/video.model");
+const LikedVideo = require("../models/likedVideo.model");
 const checkAuth = require("../middlewares/checkAuth");
 
 router.post("/:userId/:videoId/dislike", checkAuth, async (req, res) => {
@@ -11,9 +12,12 @@ router.post("/:userId/:videoId/dislike", checkAuth, async (req, res) => {
 		const foundVideo = await Video.findById({ _id: videoId });
 		const foundUser = await User.findById({ _id: userId });
 		const foundDislikedVideo = await DislikedVideo.findOne({ userId });
+		const foundLikedVideo = await LikedVideo.findOne({ userId });
 		const checkPresent =
 			foundDislikedVideo &&
 			foundDislikedVideo.videos.find((el) => String(el) === String(videoId));
+		const checkInLikedVideos =
+			foundLikedVideo && foundLikedVideo.videos.find((el) => String(el) === String(videoId));
 		if (checkPresent) {
 			console.log("Inside check present");
 			foundDislikedVideo.videos = foundDislikedVideo.videos.filter(
@@ -23,9 +27,16 @@ router.post("/:userId/:videoId/dislike", checkAuth, async (req, res) => {
 			await foundVideo.save();
 			let newDislikedVideos = await foundDislikedVideo.save();
 			newDislikedVideos = await newDislikedVideos.populate("videos").execPopulate();
-			return res.status(201).json({ likedVideos: newDislikedVideos });
+			return res.status(201).json({ likedVideos: newDislikedVideos, clickedVideo: videoId });
 		}
+
 		if (foundDislikedVideo) {
+			if (checkInLikedVideos) {
+				foundVideo.likes = foundVideo.likes - 1;
+				foundLikedVideo.videos = foundLikedVideo.videos.filter(
+					(el) => String(el) !== String(videoId)
+				);
+			}
 			foundVideo.dislikes = foundVideo.dislikes + 1;
 			await foundVideo.save();
 			foundDislikedVideo.videos.push(videoId);
@@ -34,6 +45,12 @@ router.post("/:userId/:videoId/dislike", checkAuth, async (req, res) => {
 			return res.status(201).json({ item: newDislikedVideos });
 		}
 		const videoToAdd = new DislikedVideo({ userId, videos: [videoId] });
+		if (checkInLikedVideos) {
+			foundVideo.likes = foundVideo.likes - 1;
+			foundLikedVideo.videos = foundLikedVideo.videos.filter(
+				(el) => String(el) !== String(videoId)
+			);
+		}
 		foundVideo.dislikes = foundVideo.dislikes + 1;
 		foundUser.dislikedVideos = videoToAdd;
 		await foundVideo.save();
